2fb544d6de969059a31d00c8c5845443b3771dd5
Joel Johnson <joel.johnson@g10x.com>
2025-02-25 14:26:46 +0530

    Added Page

---
+++ cypress/Pages/reqresAPI.cy.ts
@@ -0,0 +1,56 @@
+export class APIfuncs{
+
+    getRequest(url: string, expectedStatus: number) {
+        cy.request({
+            method: 'GET',
+            url: url
+        })
+        .then((response) => {
+            expect(response.status).to.eq(expectedStatus);
+        });
+    }
+    
+    postRequest(url: string, body: any, expectedStatus: number) {
+        cy.request({
+            method: 'POST',
+            url: url,
+            body: body
+        })
+        .then((response) => {
+            expect(response.status).to.eq(expectedStatus);
+        });
+    }
+    
+    putRequest(url: string, body: any, expectedStatus: number) {
+        cy.request({
+            method: 'PUT',
+            url: url,
+            body: body
+        })
+        .then((response) => {
+            expect(response.status).to.eq(expectedStatus);
+        });
+    }
+
+    delRequest(url: string, expectedStatus: number) {
+        cy.request({
+            method: 'DELETE',
+            url: url
+        })
+        .then((response) => {
+            expect(response.status).to.eq(expectedStatus);
+        });
+    }
+    
+    generateRandomIDs(count: number, min: number, max: number, exclude: number[]) {
+        const ids = [];
+        while (ids.length < count) {
+            const randomID = Math.floor(Math.random() * (max - min + 1)) + min;
+            if (!exclude.includes(randomID) && !ids.includes(randomID)) {
+                ids.push(randomID);
+            }
+        }
+        return ids;
+    }
+
+}

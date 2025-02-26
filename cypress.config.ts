60f94ea045fa7a68d4b43aa576492d2743a4b6ab
Joel Johnson <joel.johnson@g10x.com>
2025-02-26 11:28:07 +0530

    Added Cypress Cloud Integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 import * as os from "node:os";
 
 export default defineConfig({
+  projectId: 'rjwatv',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
